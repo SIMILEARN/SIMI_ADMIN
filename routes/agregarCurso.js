7fdc186f69874735bd6c53fdc83f867ac6b96465
yul-alex <43015441+yul-alex@users.noreply.github.com>
2020-08-19 14:12:28 -0500

    update perfildocente y login

--- routes/agregarCurso.js
+++ routes/agregarCurso.js
@@ -1,45 +1,14 @@
-const util = require('util');
-const db = require('../connection').Pool;
-
-// Promesas nativas
-const query = util.promisify(db.query).bind(db);
+var express = require('express');
+var router = express.Router();
+const { GetData } = require('../services/agregarCurso')
+const { PostData } = require('../services/agregarCurso')
+const { TraerData } = require('../services/agregarCurso')
 
 /* GET home page. */
-const GetData = async (req, res, next) => {
-  console.log(req.params.id)
-    const result = await query(`SELECT nombre_curso,fecha_inicio,fecha_fin, estado  FROM curso WHERE fk_docente ='${req.params.id}'`);
+router.get('/agregaCurso', GetData);
+router.post('/agregaCurso', PostData);
+router.get('/agregaCurso', TraerData);
 
-  };
-  
-  /* traer un curso */
-  const TraerData = async (req, res, next) => {
-    console.log(req.params.id)
-    try {
-      const result = await query(`SELECT pin_curso,nombre_curso,fecha_inicio,fecha_fin, estado  FROM curso WHERE pin_curso = '${req.params.id}'`);
-      res.json(result);
-    }
-    catch (error) {
-      console.log('Error =>', error);
-      res.send(error.sqlMessage);
-    }
-  };
-  
-  
-  
-  const PostData = async (req, res, next) => {
-      var {texto} = req.body;
-      try {
-        const result = await query("INSERT INTO curso (pin_curso,nombre_curso,fecha_inicio,fecha_fin) VALUES (?)", [texto]);           
-        res.json(result);
-      } catch (error) {
-        console.log('Error =>', error);
-        res.send(error.sqlMessage);
-      }
-    };
-    
-    
-    module.exports = {
-      GetData,
-      PostData,
-      TraerData
-    }
+
+
+module.exports = router;
