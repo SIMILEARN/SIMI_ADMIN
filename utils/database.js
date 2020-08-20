8d95e0804518ef742e3ecc5b175fa5deb28a136f
Yustres <58448153+Yustres@users.noreply.github.com>
2020-08-20 11:03:55 -0500

    arreglos de conexion

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var pool  = mysql.createPool({
-    Host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'simi'
+    Host     : 'us-cdbr-east-02.cleardb.com',
+    user     : 'b2982d2a9ead65',
+    password : '845d7be3',
+    database : 'heroku_c96e248066e8d6a'
 });
 
 exports.pool = pool;
